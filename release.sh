806d284e8cae68653de60be80eefe9e35d20209a
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-10 18:58:02 +0530

    Initial commit
    release.sh, to --/release/-- the builds after successful completion of "Grunt-Task(s):build", for "XOKB®".
    Usage: $ sh release.sh

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# ===========================++== XOKB® ==++==========================
+#       A manually curated dedicated xanthomonas oryzae database.
+#
+#                      GNU GENERAL PUBLIC LICENSE
+#                     http://www.gnu.org/licenses/
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
