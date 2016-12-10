a1f54e842530b1dc5ffc62f329a2681f007e9213
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-11 00:34:09 +0530

    Initial commit
    grunt-contrib-copy — Copy files and directory(s).
    Usage: $ npm install grunt-contrib-copy --save-dev | Help: https://github.com/gruntjs/grunt-contrib-copy

---
+++ tasks/options/copy.js
@@ -0,0 +1,30 @@
+/*!
+ * ----------
+ * XOKB®-Data
+ * ----------
+ * Data of XOKB — A manually curated dedicated xanthomonas oryzae database.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 10-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-copy
+ * @build     : SEED™ — Örebro
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "XOKB®-Data".
+ * ___________________________________________________________________________
+ */
