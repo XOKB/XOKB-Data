748e02655342941f1bfd280615648f773e01e686
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-11 14:58:03 +0530

    Initial commit
    Registering a {loadConfig} function to load the different option files, based on their names.
    Using: glob — a little globber | https://github.com/isaacs/node-glob

--- Gruntfile.js
+++ Gruntfile.js
@@ -207,3 +207,20 @@ module.exports = function(grunt) {
   // -----------------------------------------------------
   // Display the elapsed execution time of grunt tasks.
   require('time-grunt')(grunt);
+  
+  // Utility to load the different option files,
+  // based on their names —————————————————————.
+  function loadConfig(path) {
+    var object = {};
+    var key;
+    
+    // Perform a synchronous glob search.
+    // glob.sync(pattern, [options]).
+    glob.sync('*', {
+      cwd: path
+    }).forEach(function(option) {
+      key = option.replace(/\.js$/,'');
+      object[key] = require(path + option);
+    });
+    return object;
+  }
